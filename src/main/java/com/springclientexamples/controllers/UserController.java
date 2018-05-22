f6b15963b5b065e66090a43a3a611681fd0c65ed
Irinej Stjepanović <irinejs@gmail.com>
2018-05-22 17:26:29 +0200

    Going reactive

--- src/main/java/com/springclientexamples/controllers/UserController.java
+++ src/main/java/com/springclientexamples/controllers/UserController.java
@@ -3,7 +3,6 @@ package com.springclientexamples.controllers;
 import com.springclientexamples.services.ApiService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.server.ServerWebExchange;
@@ -25,7 +24,7 @@ public class UserController {
     @PostMapping("/users")
     public String formPost(Model model, ServerWebExchange serverWebExchange) {
 
-        MultiValueMap<String, String> map = serverWebExchange.getFormData().block();
+      /*  MultiValueMap<String, String> map = serverWebExchange.getFormData().block();
 
         Integer limit = new Integer(map.get("limit").get(0));
 
@@ -35,7 +34,13 @@ public class UserController {
             limit = 10;
         }
 
-        model.addAttribute("users", apiService.getUsers(limit));
+        model.addAttribute("users", apiService.getUsers(limit));*/
+
+        model.addAttribute("users",
+                apiService
+                        .getUsers(serverWebExchange
+                        .getFormData()
+                        .map(data -> new Integer(data.getFirst("limit")))));
 
         return "userlist";
 
--- src/main/java/com/springclientexamples/services/ApiService.java
+++ src/main/java/com/springclientexamples/services/ApiService.java
@@ -1,10 +1,14 @@
 package com.springclientexamples.services;
 
 import com.api.domain.User;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 public interface ApiService {
 
     List<User> getUsers(Integer limit);
+
+    Flux<User> getUsers(Mono<Integer> limit);
 }
